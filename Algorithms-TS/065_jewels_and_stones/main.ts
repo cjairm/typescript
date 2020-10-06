c6e1a746460055c99fb40518408e45406bd207ba
Carlos Mendez <carlos@hadaelectronics.com>
2020-10-06 11:44:58 -0500

    adding Jewels and Stones

---
+++ Algorithms-TS/065_jewels_and_stones/main.ts
@@ -0,0 +1,25 @@
+function numJewelsInStones(J: string, S: string): number {
+    const jewels: string[] = J.split("");
+    const stones: string[] = S.split("");
+
+    let i: number = 0;
+    let j: number = 0;
+    let currJ: string = "";
+    let currS: string = "";
+    let count: number = 0;
+
+    while (jewels.length > i) {
+        currJ = jewels[i];
+        currS = stones[j];
+
+        if (currJ === currS) count++;
+
+        j++;
+        if (j >= stones.length) {
+            i++;
+            j = 0;
+        }
+    }
+
+    return count;
+}
